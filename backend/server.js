c165ad6eecaa1cd0e4c7c988fcd6b5dea58218c2
Omoghene Ernest <126840281+dahGuru@users.noreply.github.com>
2025-01-23 17:32:12 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,12 @@ import cookieParser from "cookie-parser";
 import cors from 'cors'
 import morgan from 'morgan';
 import helmet from 'helmet';
-import path from "path";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
@@ -26,14 +25,6 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ success: false, message: 'Something went wrong! Please try again later.' });
 });
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.use(morgan('dev'));  // For development logs
 app.use(helmet()); // Adds security-related HTTP headers
